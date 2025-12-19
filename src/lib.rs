2de4af0addb1156c0c2a5ef6016b81439f719f69
Garikai <gijimaga@grinnell.edu>
2025-12-19 14:08:17 -0600

    Initializations and dependencies

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! A high-performance text CRDT library for collaborative editing.
+//!
+//! This library implements a Sequence CRDT (Replicated Growable Array)
+//! that enables real-time collaborative text editing with eventual consistency.
+//!
+//! # Example
+//!
+//! ```rust
+//! // TODO: Add examples as implementation progresses
+//! ```
+
+// Module declarations will be added in subsequent PRs
+// mod op_id;
+// mod node;
+// mod document;
